d7e69112d0bfeff077788494ac0fd64576465fdb
Vitor Ribeiro <vitorhugomelo5@gmail.com>
2022-09-20 12:32:26 -0300

    :bookmark: Final v1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,8 @@
 client_script "@vrp/lib/lib.lua" --Para remover esta pendencia de todos scripts, execute no console o comando "uninstall"
 
+author 'Vitor Ribeiro'
+description 'Faction NPC Saller FiveM / vRP Framework'
+repository 'https://github.com/vitorRibeiro7/striXEstoqueAmmo'
 fx_version "bodacious"
 game "gta5"
 
